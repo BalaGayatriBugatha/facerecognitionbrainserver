d7b8d516e38ed90ed9170ce0297430b8a0731701
Bala Gayatri Bugatha <balagayathri9@gmail.com>
2019-09-30 14:42:14 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors=require('cors');
+const app=express();
+const register=require('./controllers/register');
+const signin=require('./controllers/signin');
+const profile=require('./controllers/profile');
+const image=require('./controllers/image');
+const db=require('knex')({
+  client: 'pg',
+  connection: {
+    host : 'localhost',
+    user : 'postgres',
+    password : 'gayi',
+    database : 'test'
+  }
+});
+db.select('*').from('users').then(data=>{console.log(data)});
+app.use(bodyParser.json());
+app.use(cors());
+
+
+
+app.get('/',(req,res)=>{ 
+	res.send(database.users);
+})
+
+app.post('/signin',(req,res)=>{signin.handleSignin(req,res,db,bcrypt)})
+	
+app.post('/register',(req,res)=>{register.handleRegister(req,res,db,bcrypt)})
+
+app.get('/profile/:id',(req,res)=>{profile.handleProfile(req,res,db)})
+
+app.put('/image',(req,res)=>{image.handleImage(req,res,db)})
+
+app.post('/imageurl',(req,res)=>{image.handleApiCall(req,res)})
+
+app.listen(3001,()=>
+{
+	console.log("app server mode on")
+})
